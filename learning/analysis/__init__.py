5bca9bba68acbec9334117263ecd4575f924a3ef
xla145 <1379960679@qq.com>
2020-12-03 11:05:35 +0800

    创建一些基础的目录

---
+++ learning/analysis/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Time: 2020/12/3 11:04
+# Author: xula
+# Mail: 1379960679@qq.com
+
+"""
+
+该包下主要写一些数据分析的代码
+
+"""
---
+++ learning/base/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Time: 2020/12/3 11:03
+# Author: xula
+# Mail: 1379960679@qq.com
